1dff2ed2fd70f4ffbc45dc789417cd4fbcc58b0f
Rohith <rohith832860@gmail.com>
2024-02-21 23:41:59 +0530

    added datatype sizes

---
+++ Datatype.java
@@ -0,0 +1,14 @@
+class Datatype
+{
+    public static void main(String args[])
+    {
+        System.out.println("Byte: "+Byte.SIZE/8);
+        System.out.println("Short: "+Short.SIZE/8);
+        System.out.println("int: "+Integer.SIZE/8);
+        System.out.println("Long: "+Long.SIZE/8);
+        System.out.println("Float: "+Float.SIZE/8);
+        System.out.println("Double: "+Double.SIZE/8);
+        System.out.println("Char: "+Character.SIZE/8);
+        
+    }
+}
