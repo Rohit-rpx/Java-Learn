099052d1c75147148f21a36ef45e9720da68c748
Rohith <rohith832860@gmail.com>
2024-02-21 23:13:16 +0530

    Program2 -- float

--- Program2.java
+++ Program2.java
@@ -2,8 +2,8 @@ class Program2
 {
     public static void main(String args[])
     {
-        String num1="5",num2="4";
-        String res=String.join("+",num1,num2);
+        float num1=5.8f,num2=9.4f;
+        float res = num1+num2;
         System.out.println(res);
     }    
 }
