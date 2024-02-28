2a9cdc250615619842a787d82485314ea625e285
Rohith <rohith832860@gmail.com>
2024-02-28 23:42:49 +0530

    Added loop statements

---
+++ Dowhile.java
@@ -0,0 +1,12 @@
+class Dowhile
+{
+    public static void main(String args[])
+    {
+        int i=0;
+        do
+        {
+            System.out.println(i+" X 5 = "+(i*5));
+            i++;
+        }while(i<=10);
+    }    
+}
---
+++ Foreach.java
@@ -0,0 +1,11 @@
+class Foreach
+{
+    public static void main(String args[])
+    {
+        int ar[]={23,34,45,56,67,78};
+        for(int i : ar)
+        {
+            System.out.println(i);
+        }
+    }    
+}
